99b30fa2db5492e2971e735a402b8103cae4488e
Muhammad Raid Zakwan <90046024+zmy16@users.noreply.github.com>
2024-10-01 13:43:05 +0700

    Update Untitled4.cpp

--- D/Untitled4.cpp
+++ D/Untitled4.cpp
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int T; // Jumlah test case
-    scanf("%d", &T); // Baca jumlah test case
+    int T;
+    scanf("%d", &T);
 
-    int N[T], M[T]; // Array untuk menyimpan N dan M untuk setiap test case
+    int N[T], M[T];
 
-    // Membaca seluruh input N dan M terlebih dahulu
     for (int i = 0; i < T; i++) {
         scanf("%d %d", &N[i], &M[i]);
     }
 
-    // Menghasilkan output setelah seluruh input dibaca
     for (int i = 0; i < T; i++) {
-        long long acquaintances = (long long) N[i] * M[i]; // Hitung jumlah perkenalan
-
-        // Cek apakah jumlah perkenalan ganjil atau genap
+        long long acquaintances = (long long) N[i] * M[i];
+        
         if (acquaintances % 2 == 0) {
             printf("Case #%d: Party time!\n", i + 1);
         } else {
